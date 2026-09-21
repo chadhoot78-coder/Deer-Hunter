e8a4d04f99715c8082e4dc688bc367dfa2d2e1be
chadhoot78-coder <chadhoot78@gmail.com>
2026-09-21 17:33:31 -0500

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,5 +1,5 @@
 // Bump CACHE every time index.html changes so phones pick up the new version.
-const CACHE = 'whitetail-v1';
+const CACHE = 'whitetail-v2';
 const ASSETS = ['./', './index.html', './manifest.json', './icon.png'];
 
 self.addEventListener('install', e => {
